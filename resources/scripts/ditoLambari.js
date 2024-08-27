3f03891eed070e089e9690d074aab3468b8a4e40
Bruno Faria <developer.brunofaria@gmail.com>
2024-08-26 21:56:49 -0400

    update project

--- resources/scripts/ditoLambari.js
+++ resources/scripts/ditoLambari.js
@@ -73,30 +73,70 @@ $(function() {
             $('#productRow0').find('select').append('<option value="' + prato.id + '">' + prato.title + '</option>')
         });
         /* validate int values */
-        $(document).on('input', '.quantidade', function() {
+        $(document).on('input change', '.quantidade', function() {
+            if (parseInt($(this).val()) < 1 ) {
+                $(this).val('1')
+            }
             $(this).val($(this).val().replace(/[^0-9]/g, ''));
+        });
+        $(document).on('click', function(e) {
+            var quantidade = $(".quantidade");
+            if(!quantidade.is(e.target) && quantidade.has(e.target).length === 0) {
+                // quantidade.hide(); 
+                $('.quantidade').each(function(index, quantity) {
+                    if (parseInt($(quantity).val()) < 1 ) {
+                        $(quantity).val('1')
+                    }
+                });
+            }
         });
         /* index of dynamic product create */ 
         let productRowControl = 0;
 
         $(document).on('click', '.new-item', function(e){
             e.preventDefault;
+            
             let newRow = $('#productExample').html();
             $('.itens').append('<div id="productRow'+(productRowControl+1)+'" class="productRow form-group"></div>')
             $('#productRow'+(productRowControl+1)).html(newRow);
+            
             pratos.forEach(prato => {
                 $('#productRow'+(productRowControl+1)).find('select').append('<option value="' + prato.id + '">' + prato.title + '</option>')
             });
+            
             $('#productRow'+(productRowControl)).find('.new-item').remove();
             $('#productRow'+(productRowControl+1)).append('<button class="new-item form-control mt-3 btn btn-primary">Adicionar Produto</button>');
+            
             productRowControl ++;
+            
+            reloadValues();
         });
 
-         $(document).on('click', '.delete-item', function(e){
+        $(document).on('click', '.delete-item', function(e){
             e.preventDefault;
-            let id = $(this).closest('div.productRow').attr('id').substring(10);
+
+            let id = parseInt($(this).closest('div.productRow').attr('id').substring(10));
             $(this).closest('div.productRow').remove();
+            
+            let products = $('.productRow');
+            for (let i = id; i < products.length; i++) {
+                const element = $(products[i]).attr('id', 'productRow'+id);
+                id++;
+            }
+            productRowControl --;
+            reloadValues();
         });
+
+        function reloadValues(){
+            let total = [];
+            $('.productRow').each(function(index, product) {
+                let productId = parseInt($(this).find('select[name="products[]"]').val());
+                let productDBValues = pratos.find(prato => prato.id === productId);
+
+                total += productDBValues.value*parseInt($(product).find('input.quantidade').val());
+                console.log(total);
+            });
+        }
 
         // função javascript puro que foi utilizada console.log(pratos.find(prato => prato.id === 3).title);
         console.log(pratos.find(prato => prato.id === 3).title)
